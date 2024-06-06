d7a368efc7aef3182145fbd38011a2af45b3094b
Isaias Xavier Santana <isaiasxavier@outlook.com>
2024-06-06 20:15:36 +0200

    Solution by Isaias Xavier

--- database/migrations/task5/2021_11_09_083051_create_another_users_table.php
+++ database/migrations/task5/2021_11_09_083051_create_another_users_table.php
@@ -1,36 +1,38 @@
 <?php
-
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
-
-class CreateAnotherUsersTable extends Migration
-{
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
+    
+    use Illuminate\Database\Migrations\Migration;
+    use Illuminate\Database\Schema\Blueprint;
+    use Illuminate\Support\Facades\Schema;
+    
+    class CreateAnotherUsersTable extends Migration
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
-        });
+        /**
+         * Run the migrations.
+         *
+         * @return void
+         */
+        public function up()
+        {
+            if (!Schema ::hasTable('users')) {
+                Schema ::create('users', function (Blueprint $table) {
+                    $table -> id();
+                    $table -> string('name');
+                    $table -> string('email') -> unique();
+                    $table -> timestamp('email_verified_at') -> nullable();
+                    $table -> string('password');
+                    $table -> rememberToken();
+                    $table -> timestamps();
+                });
+            }
+        }
+        
+        /**
+         * Reverse the migrations.
+         *
+         * @return void
+         */
+        public function down()
+        {
+            Schema ::dropIfExists('users');
+        }
     }
-
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('users');
-    }
-}
